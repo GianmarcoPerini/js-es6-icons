d24894e71608d8444be5cf2c4ccaf3f3f86490f1
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-14 01:14:56 +0200

    stile

--- main.js
+++ main.js
@@ -2,100 +2,103 @@ const icon = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
-		type: 'animal',
+		type: 'Animal',
 		family: 'fas'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'Vegetable',
 		family: 'fas'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'Vegetable',
 		family: 'fas'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'Vegetable',
 		family: 'fas'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'Vegetable',
 		family: 'fas'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'User',
 		family: 'fas'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'User',
 		family: 'fas'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'User',
 		family: 'fas'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'User',
 		family: 'fas'
 	}
 ];
+
+
+
 const dropDown = document.querySelector('#drop-down')
 const box = document.querySelector('#box');
 
@@ -109,15 +112,21 @@ const filterMenu = () => {
     })
 }
 
-
 const displayElement = () => {
 		dropDown.addEventListener('change', (event)=>{
 			let writeHTML = filtered(event.target.value)
 			let temporaryHTML = ''
 			writeHTML.forEach(element => {
-			if(event.target.value == `${element.type}`){
+			if(event.target.value == 'All'){
 				temporaryHTML += 
-				`<div class="dai">
+				`<div class="card card-obj">
+					<i class="${element.family} ${element.prefix}${element.name}"></i>
+					<p>${element.name.toUpperCase().split('-').join(' ')}</p>
+				</div>`
+				box.innerHTML = temporaryHTML;
+			}else if (event.target.value == `${element.type.charAt(0).toUpperCase() + element.type.slice(1)}`){
+				temporaryHTML += 
+				`<div class="card card-obj">
 					<i class="${element.family} ${element.prefix}${element.name}"></i>
 					<p>${element.name.toUpperCase().split('-').join(' ')}</p>
 				</div>`
@@ -127,16 +136,26 @@ const displayElement = () => {
 	})
 }
 
-
-
-
-
-
 const filtered = (toFilter) => {
+	if(toFilter == 'All'){
+		return icon
+	} 
 	return icon.filter(el => el.type == toFilter)
 }
 
+const defaultHTML = () => {
+	icon.forEach(element => {
+		let tempDefault = 
+		`<div class="card card-obj">
+			<i class="${element.family} ${element.prefix}${element.name}"></i>
+			<p class="mb-0">${element.name.toUpperCase().split('-').join(' ')}</p>
+		</div>`
+		box.innerHTML += tempDefault
+	})
+}
 
+
+defaultHTML()
 
 filterMenu()
 
